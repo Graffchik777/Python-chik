9910c7f7f61dd87116854b8cd873a7e3ce3355c3
Graffchik777 <legione77@mail.ru>
2022-10-08 21:25:13 +0300

    add HomeWork11

---
+++ HomeWork11.py
@@ -0,0 +1,6 @@
+import random
+ 
+list_1 = [1, 4, 5, 6, 3, 9, 31, 2]
+print ("Сначала был хаос такой: " + str(list_1))
+random.shuffle(list_1)
+print (("Потом стал хаос вот такой: " +  str(list_1))) 
