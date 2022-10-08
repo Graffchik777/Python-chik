419f9cb2c92c4a6f9be6c3d9e34991e728ca99e8
Graffchik777 <legione77@mail.ru>
2022-10-08 19:03:29 +0300

    add HomeWork8 говорят что функциональность это плохо, но мне понравилось))

--- HomeWork7.py
+++ HomeWork7.py
@@ -3,12 +3,26 @@
 
 
 
-print("Забей координатки точек А:  ")
-x1 = int(input("Первый X: "))
-y1 = int(input("Первый Y: "))
-print("Введи координаты точек B:")
-x2 = int(input("Второй X: "))
-y2 = int(input("Второй Y: "))
+# print("Забей координатки точек А:  ")
+# x1 = int(input("Первый X: "))
+# y1 = int(input("Первый Y: "))
+# print("Введи координаты точек B:")
+# x2 = int(input("Второй X: "))
+# y2 = int(input("Второй Y: "))
 
-from math import sqrt
-print("Расстояние определяем по теореме Пифагорыча =",round(sqrt((x1 - x2)**2 + (y1 - y2)**2),2)) 
+# from math import sqrt
+# print("Расстояние определяем по теореме Пифагорыча =",round(sqrt((x1 - x2)**2 + (y1 - y2)**2),2)) 
+
+# Number = int(input('Введите число N: '))
+
+# result = {}
+# for key in range(1, Number+1):
+#     result[key] = key*3 +1
+
+# print(result)
+
+
+n = int(input("Введите n: "))
+
+lst = [round((1+1/i)**i, 3) for i in range(1, n+1)]
+print(f'Последовательность: {lst}\nСумма: {round(sum(lst), 3)}')
